0f0ead2a8956ffaf238d9f726449c5ae591c8d70
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2023-01-23 19:03:09 +0100

    Add basic text rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
+#include <iostream>
+
+#include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Text.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
 
@@ -6,6 +10,14 @@ int main()
 {
     auto window = sf::RenderWindow{ sf::VideoMode{{400, 200}}, "WhisperSFML" };
     window.setFramerateLimit(30);
+
+    auto font = sf::Font{};
+    if (!font.loadFromFile("res/LinLibertine_R.ttf"))
+    {
+        std::cerr << "Failed to load font!" << std::endl;
+    }
+
+    auto text = sf::Text{ "Hello World!", font, 21 };
 
     while (window.isOpen())
     {
@@ -18,6 +30,7 @@ int main()
         }
 
         window.clear();
+        window.draw(text);
         window.display();
     }
 }
